74c9470b2aadba853730a60a2dcdc36efdcd1ae7
Juaen Cruz <105850236+jmviradukunda@users.noreply.github.com>
2022-05-30 09:53:37 -0700

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>J.M.V_Cruz</title>
+</head>
+<body>
+    <form action="" method="post">
+    <header class="text-white p-3 text-center"style="background-color:blue;">
+<h4>IREMBO | Practical CAT</h4>
+</header>
+
+
+<div class="d-flex justify-content-center p-5 " style="height:80vh;">
+
+<div class="card px-5 pt-4 position-relative text-center w-70">
+           <img src="jmv.jpeg" class="rounded-circle mx-5 " style="width: 80px; height: 80px;"><br>
+           <h5>TUYISHIMIRE Peter</h5><br>
+           <p>tuyishimirepeter06@gmail.com</p>
+           <div class="my-5">
+                <button type="submit" name="facebook" class="form-control btn btn-blue fw-bold text-white mt-5" style="background-color:blue;font-size:20px;">
+                    <i class="text-white" aria-hidden="true" style="font-size:10px;"></i>
+                    Logout
+                </button>
+           </div>
+        </div>
+</div>
+</form>
+</body>
+</html>
